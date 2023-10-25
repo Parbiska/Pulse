dec81cdd0300b8be2be3d86399d96047237ef549
Demid <parbiska@gmail.com>
2023-10-25 16:23:05 +0800

    Initial dist subtree commit

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -7,20 +7,26 @@ import autoprefixer from 'gulp-autoprefixer';
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
-    return app.gulp.src(app.path.src.scss, { souremaps: true })
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }))
-        .pipe(autoprefixer({
-            grid: true,
-            overrideBrowserslist: ["last 3 versions"],
-            cascade: true,
-        }))
-        .pipe(app.gulp.dest(app.path.build.css))
-        .pipe(cleanCss())
-        .pipe(rename({
-            extname: ".min.css"
-        }))
-        .pipe(app.gulp.dest(app.path.build.css))
-        .pipe(app.plugins.browsersync.stream())
-}
+	return app.gulp
+		.src(app.path.src.scss, { souremaps: true })
+		.pipe(
+			sass({
+				outputStyle: 'expanded',
+			})
+		)
+		.pipe(
+			autoprefixer({
+				grid: true,
+				overrideBrowserslist: ['last 3 versions'],
+				cascade: true,
+			})
+		)
+		.pipe(cleanCss())
+		.pipe(
+			rename({
+				extname: '.min.css',
+			})
+		)
+		.pipe(app.gulp.dest(app.path.build.css))
+		.pipe(app.plugins.browsersync.stream());
+};
